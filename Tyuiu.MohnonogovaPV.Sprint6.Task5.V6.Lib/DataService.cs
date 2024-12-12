be727f5b6180519cab8fd34abe61127bb0fde3ad
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-12-12 11:59:48 +0500

    Task5

---
+++ Tyuiu.MohnonogovaPV.Sprint6.Task5.V6.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using Tyuiu.MohnonogovaPV.Sprint6.Task5.V6.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint6.Task5.V6.Lib
+{
+    public class DataService : ISprint1Task5V6
+    {
+        public int len = 0;
+        public double[] LoadFromDataFile(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    len++;
+                }
+            }
+            double[] numsArray = new double[len];
+
+            int index = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    numsArray[index] = Math.Round(Convert.ToDouble(line), 3);
+                    index++;
+                }
+            }
+
+            numsArray = numsArray.Where(val => val % 3 == 0).ToArray();
+            return numsArray;
+        }
+    }
+}
---
+++ Tyuiu.MohnonogovaPV.Sprint6.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.MohnonogovaPV.Sprint6.Task5.V6.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint6.Task5.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckLoadFromDataFile()
+        {
+            double[] valueWaitArray;
+            valueWaitArray = new double[1];
+            valueWaitArray[0] = -12;
+
+        }
+    }
+}
