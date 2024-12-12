bbaa8a014c5e17ec2ef6b7a482571eca3e57429e
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-12-12 19:30:22 +0500

    Task5

--- Tyuiu.MohnonogovaPV.Sprint6.Task5.V6.Test/DataServiceTest.cs
+++ Tyuiu.MohnonogovaPV.Sprint6.Task5.V6.Test/DataServiceTest.cs
@@ -7,6 +7,7 @@ namespace Tyuiu.MohnonogovaPV.Sprint6.Task5.V6.Test
         [TestMethod]
         public void CheckLoadFromDataFile()
         {
+            var path = @"C:\DataSprint6\InPutFileTask5V6.txt";
             double[] valueWaitArray;
             valueWaitArray = new double[1];
             valueWaitArray[0] = -12;
